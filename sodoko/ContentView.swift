ed4c80c8913253fa09925ca576b42bd52f1ccb88
vdotup <v.up@live.com>
2022-02-25 21:28:15 +0300

    Initial Commit

---
+++ sodoko/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sodoko
+//
+//  Created by Abdurrahman Alfudeghi on 25/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sodoko/sodokoApp.swift
@@ -0,0 +1,17 @@
+//
+//  sodokoApp.swift
+//  sodoko
+//
+//  Created by Abdurrahman Alfudeghi on 25/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct sodokoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
